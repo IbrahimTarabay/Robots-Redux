d9806c4e2cf5398259315c47fabc207bdd30cf22
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-29 14:31:33 +0200

    Updates

---
+++ precache-manifest.6ee07e06b1a3bc7d6d10a530e65a614e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1bf36e5226e3db178a26fee447a45f",
+    "url": "/Robots-Redux/index.html"
+  },
+  {
+    "revision": "4e224fa88ee48f476364",
+    "url": "/Robots-Redux/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "34fe4a7eaec1d4bff4d6",
+    "url": "/Robots-Redux/static/css/main.2dbf7fa3.chunk.css"
+  },
+  {
+    "revision": "4e224fa88ee48f476364",
+    "url": "/Robots-Redux/static/js/2.d374cb4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Robots-Redux/static/js/2.d374cb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fe4a7eaec1d4bff4d6",
+    "url": "/Robots-Redux/static/js/main.c586b926.chunk.js"
+  },
+  {
+    "revision": "68318445d666adc194df",
+    "url": "/Robots-Redux/static/js/runtime-main.acf1e9c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-Redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
