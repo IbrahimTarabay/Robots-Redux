6ab3b337144d715a247c3b6be0b60c5d28806abf
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-04-04 16:36:24 +0200

    return a promise in actions.js

--- src/redux/actions.test.js
+++ src/redux/actions.test.js
@@ -1,33 +1,81 @@
-import * as actions from './actions';
 import {
     CHANGE_SEARCHFIELD,
     REQUEST_ROBOTS_PENDING,
     REQUEST_ROBOTS_SUCCESS,
     REQUEST_ROBOTS_FAILED
-   } from './constants';
-
-import fetchMock from 'fetch-mock';   
-
-import configureMockStore from 'redux-mock-store';
-import thunkMiddleware from 'redux-thunk';   
-
-const mockStore = configureMockStore([thunkMiddleware]);
-
-it('should create an action to search robots', () => {
-  const text = 'wooo'
-  const expectedAction = {
-    type: CHANGE_SEARCHFIELD,
-    payload: text
-  }
-  expect(actions.setSearchField(text)).toEqual(expectedAction)
-})
-
-it('handles requesting robots API', () => {
-  const store = mockStore();
-  store.dispatch(actions.requestRobots())
-  const action = store.getActions();
-  const expectedAction = {
-    type: REQUEST_ROBOTS_PENDING
-  }
- expect(action[0].toEqual(expectedAction))
-})
+  } from "./constants";
+  
+  import fetchMock from "fetch-mock";
+  
+  import configureMockStore from "redux-mock-store";
+  import thunkMiddleware from "redux-thunk";
+  
+  import * as actions from "./actions";
+  
+  const mockStore = configureMockStore([thunkMiddleware]);
+  
+  describe("setSearchField", () => {
+    it("should create an action to search robots", () => {
+      const text = "woo";
+      const expectedAction = {
+        type: CHANGE_SEARCHFIELD,
+        payload: text
+      };
+  
+      expect(actions.setSearchField(text)).toEqual(expectedAction);
+    });
+  });
+  
+  describe("requestRobots", () => {
+    afterEach(() => {
+      fetchMock.restore();
+    });
+  
+    it("handles requesting robots API", () => {
+      const store = mockStore();
+      store.dispatch(actions.requestRobots());
+      const action = store.getActions();
+      const expectedAction = {
+        type: REQUEST_ROBOTS_PENDING
+      };
+      expect(action[0]).toEqual(expectedAction);
+    });
+  
+    it("sends API call data to store", () => {
+      //expect.assertions(1);
+      const store = mockStore();
+  
+      fetchMock.mock("glob:https://jsonplaceholder.typicode.com/users", {
+        data: { name: "Jim" }
+      });
+  
+      const expectedAction = {
+        type: REQUEST_ROBOTS_SUCCESS,
+        payload: { data: { name: "Jim" } }
+      };
+  
+      return store.dispatch(actions.requestRobots()).then(() => {
+        expect(store.getActions()[1]).toEqual(expectedAction);
+      });
+    });
+  
+    it("sends errors to store", () => {
+      //expect.assertions(1);
+      const store = mockStore();
+  
+      fetchMock.mock("glob:https://jsonplaceholder.typicode.com/users", {
+        status: "400",
+        throws: new TypeError("Failed to fetch")
+      });
+  
+      const expectedAction = {
+        type: REQUEST_ROBOTS_FAILED,
+        payload: new TypeError("Failed to fetch")
+      };
+  
+       return store.dispatch(actions.requestRobots()).then(() => {
+        expect(store.getActions()[1]).toEqual(expectedAction);
+        
+      });
+    });
+  });
